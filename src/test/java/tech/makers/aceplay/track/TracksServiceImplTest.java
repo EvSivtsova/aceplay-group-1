102d1cdaad0f6b556a476ef2b1734afb4082d14c
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-28 15:07:16 +0000

    updateTrack_UpdatesTitleAndArtist

--- src/test/java/tech/makers/aceplay/track/TracksServiceImplTest.java
+++ src/test/java/tech/makers/aceplay/track/TracksServiceImplTest.java
@@ -7,6 +7,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.MalformedURLException;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -63,5 +66,21 @@ public class TracksServiceImplTest {
                 tracksService.validateAndSaveTrack(mockTrack));
 
         assertEquals("Track artist cannot be empty", exception.getMessage());
+    }
+
+    @Test
+    public void updateTrack_UpdatesTitleAndArtist()
+            throws IllegalArgumentException, MalformedURLException {
+        when(mockTrack.getArtist()).thenReturn("New Artist");
+        when(mockTrack.getTitle()).thenReturn("New Title");
+
+        Track originalTrack = new Track("Title", "Artist", "https://example.org/");
+        Long mockId = Long.valueOf(1);
+        when(mockRepository.findById(mockId)).thenReturn(Optional.of(originalTrack));
+
+        tracksService.updateTrack(mockId, mockTrack);
+        assertEquals(mockTrack.getArtist(), originalTrack.getArtist());
+        assertEquals(mockTrack.getTitle(), originalTrack.getTitle());
+        verify(mockRepository).save(originalTrack);
     }
 }
