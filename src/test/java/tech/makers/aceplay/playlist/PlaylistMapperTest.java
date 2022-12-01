4ec6f6b9d053ef82679688ed1d1bf7bd5d3cec39
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-12-01 15:56:48 +0000

    Update PlaylistMapperTest - dtoToPlaylist

--- src/test/java/tech/makers/aceplay/playlist/PlaylistMapperTest.java
+++ src/test/java/tech/makers/aceplay/playlist/PlaylistMapperTest.java
@@ -1,66 +1,57 @@
 package tech.makers.aceplay.playlist;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tech.makers.aceplay.playlist_track.PlaylistTrack;
+import tech.makers.aceplay.track.Track;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class PlaylistMapperTest {
-
-    private final PlaylistMapper playlistMapper = new PlaylistMapper();
-
-    private final Set<PlaylistTrack> mockPlaylistTracks = new HashSet<>();
-
-    @BeforeEach
-    public void setup() {
-        PlaylistTrack mockPlaylistTrack1 = mock(PlaylistTrack.class);
-        PlaylistTrack mockPlaylistTrack2 = mock(PlaylistTrack.class);
-        mockPlaylistTracks.add(mockPlaylistTrack1);
-        mockPlaylistTracks.add(mockPlaylistTrack2);
-    }
-
-    @Test
-    void playlistToDto_ReturnsPlaylistDtoMatchingOriginalPlaylist() {
-        Playlist originalPlaylist = new Playlist("Name");
-        originalPlaylist.setTracks(mockPlaylistTracks);
-        PlaylistDto returnedPlaylistDto = playlistMapper.playlistToDto(originalPlaylist);
-
-        assertThat(returnedPlaylistDto).usingRecursiveComparison().isEqualTo(originalPlaylist);
-
-//        Long mockId = 1L;
+//    @Test
+//    public void playlistToDto_RealObjects_ReturnsPlaylistDtoMatchingOriginalPlaylist() throws MalformedURLException {
+//        Set<PlaylistTrack> playlistTracks = new HashSet<>();
+//        PlaylistTrack mockPlaylistTrack = mock(PlaylistTrack.class);
+//        playlistTracks.add(mockPlaylistTrack);
 //
-//        Playlist expectedPlaylist = mock(Playlist.class);
-//        when(expectedPlaylist.getId()).thenReturn(mockId);
-//        when(expectedPlaylist.getName()).thenReturn("name");
-//        when(expectedPlaylist.getTracks()).thenReturn(mockTracks);
+//        Track track = new Track("Artist", "Name", new URL("http://www.fgfgfgd.com"));
+//        when(mockPlaylistTrack.getTrack()).thenReturn(track);
+//
+//        Playlist originalPlaylist = new Playlist("Name");
+//        originalPlaylist.setTracks(playlistTracks);
 //
 //        PlaylistMapper playlistMapper = new PlaylistMapper();
-//        PlaylistDto actual = playlistMapper.playlistToDto(expectedPlaylist);
+//        PlaylistDto returnedPlaylistDto = playlistMapper.playlistToDto(originalPlaylist);
 //
-//        assertEquals(expectedPlaylist.getId(), actual.getId());
-//        assertEquals(expectedPlaylist.getName(), actual.getName());
-//        assertEquals(expectedPlaylist.getTracks(), actual.getTracks());
+//        assertThat(returnedPlaylistDto).usingRecursiveComparison().isEqualTo(originalPlaylist);
+//    }
+
+    @Test
+    public void dtoToPlaylist_MocksOnly_ReturnsPlaylistMatchingOriginalPlaylistDto() {
+        PlaylistDto expectedPlaylist = mock(PlaylistDto.class);
+        when(expectedPlaylist.getName()).thenReturn("name");
+
+        PlaylistMapper playlistMapper = new PlaylistMapper();
+        Playlist actual = playlistMapper.dtoToPlaylist(expectedPlaylist);
+
+        assertEquals(expectedPlaylist.getName(), actual.getName());
     }
 
     @Test
-    void dtoToPlaylist_ReturnsPlaylistMatchingOriginalPlaylistDto() {
+    public void dtoToPlaylist_RealObjects_ReturnsPlaylistMatchingOriginalPlaylistDto() {
         PlaylistDto originalPlaylistDto = new PlaylistDto("Name");
+
+        PlaylistMapper playlistMapper = new PlaylistMapper();
         Playlist returnedPlaylist = playlistMapper.dtoToPlaylist(originalPlaylistDto);
 
         assertThat(returnedPlaylist).usingRecursiveComparison()
                 .ignoringFields("owner").isEqualTo(originalPlaylistDto);
-
-//        PlaylistDto expectedPlaylist = mock(PlaylistDto.class);
-//        when(expectedPlaylist.getName()).thenReturn("name");
-//
-//        PlaylistMapper playlistMapper = new PlaylistMapper();
-//        Playlist actual = playlistMapper.dtoToPlaylist(expectedPlaylist);
-//
-//        assertEquals(expectedPlaylist.getName(), actual.getName());
     }
 }
